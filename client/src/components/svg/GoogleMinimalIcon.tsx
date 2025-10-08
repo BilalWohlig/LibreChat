d134717b4f0fb3163e3843193286c44a87052740
Bilal2912 <bilalsani2912@gmail.com>
2025-10-08 11:37:28 +0530

    changes

--- client/src/components/svg/GoogleMinimalIcon.tsx
+++ client/src/components/svg/GoogleMinimalIcon.tsx
@@ -3,10 +3,8 @@ import { cn } from '~/utils';
 export default function GoogleMinimalIcon({ className = '' }: { className?: string }) {
   return (
     <img
-      src="../src/components/svgpath/image.svg" // Replace with the actual path to your .svg file
+      src="/assets/google-icon.svg"
       alt="Google Icon"
-      width="800"
-      height="800"
       className={cn('h-4 w-4', className)}
     />
   );
